7aba8c4e6bb1e8df0a0ad21711d322ee20718648
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-03-24 15:18:28 -0300

    reupando um bug

--- iniciante/bee1101-site-nao-aceitou.js
+++ iniciante/bee1101-site-nao-aceitou.js
@@ -1,8 +1,7 @@
 //site não aceitou minha resolução. Segue o link do problema: https://www.beecrowd.com.br/judge/pt/runs/code/26953070
 
 var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-lines = lines.map(a=>a.split(' ').sort((a,b) => a-b));
+var lines = input.split('\n').map(a=>a.split(' ').sort((a,b) => a-b));
 
 lines.forEach(a=>{
     let sum = 0;
@@ -11,11 +10,11 @@ lines.forEach(a=>{
     for (let i = parseInt(a[0]); i <= parseInt(a[1]) + 1; i++) {
         if(i <= parseInt(a[1])){
             sum += i;
-            text += `${i} `;
+            text += `${i} `
         }
         else {
             text += `Sum=${sum}`;
             return console.log(text);
         }
     }
-});
+})
